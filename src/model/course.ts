ae23005a37dbb9fddcba126d038ff68807c67b62
koreanwglasses <koreanwglasses@gmail.com>
2021-03-26 14:13:33 -0400

    Added types for Course

---
+++ src/model/course.ts
@@ -0,0 +1,29 @@
+export type Course = {
+  /**
+   * Unique identifier
+   * e.g. CS467_3
+   */
+  id: string;
+  /**
+   * The formal name of the course - the full title
+   * e.g. CS467 - Social Visualization
+   */
+  name: string;
+  description?: string;
+  creditHours: number;
+  semestersOffered: ("spring" | "fall")[];
+  concentration?: string;
+  /**
+   * An array of requirement “tags” used to determine if a set
+   * of courses meets certain requirements
+   */
+  requirementsSatisfied: string[];
+  /**
+   * An array of ids of prerequisite courses to take this course
+   */
+  prereqs: string[];
+  /**
+   * A list of courses of which this class is a prerequisite
+   */
+  followupCourses: string[];
+};
