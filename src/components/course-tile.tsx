41855b4e21b27ff21d725dc655d53dc3d7a3dccb
koreanwglasses <koreanwglasses@gmail.com>
2021-03-26 15:19:34 -0400

    replace style with module css

--- src/components/course-tile.tsx
+++ src/components/course-tile.tsx
@@ -1,19 +1,9 @@
 import * as React from "react";
 import { Course } from "../model/course";
+import styles from "./course-tile.module.css";
 
 export const CourseTile: React.FC<{
   course: Course;
 }> = ({ course }) => {
-  return (
-    <div
-      style={{
-        width: "200px",
-        height: "50px",
-        borderStyle: "solid",
-        borderWidth: "1px",
-      }}
-    >
-      {course.id}
-    </div>
-  );
+  return <div className={styles.tile}>{course.id}</div>;
 };
