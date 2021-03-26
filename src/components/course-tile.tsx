380443eef8ef173d8f42bc20ded19c95831be66d
koreanwglasses <koreanwglasses@gmail.com>
2021-03-26 16:20:17 -0400

    tiles are now draggable

--- src/components/course-tile.tsx
+++ src/components/course-tile.tsx
@@ -1,9 +1,38 @@
 import * as React from "react";
+import { DraggableCore, DraggableEventHandler } from "react-draggable";
 import { Course } from "../model/course";
 import styles from "./course-tile.module.css";
 
 export const CourseTile: React.FC<{
   course: Course;
 }> = ({ course }) => {
-  return <div className={styles.tile}>{course.name}</div>;
+  const containerRef = React.useRef<HTMLDivElement>();
+  const startPosition = [0, 0] as [number, number];
+  const [position, setPosition] = React.useState<[number, number]>(
+    startPosition
+  );
+
+  const reset = () => {
+    setPosition(startPosition);
+  };
+
+  const onDrag: DraggableEventHandler = (e, { deltaX, deltaY }) => {
+    setPosition([position[0] + deltaX, position[1] + deltaY]);
+  };
+
+  const dragStop: DraggableEventHandler = (e) => {
+    reset();
+  };
+
+  return (
+    <DraggableCore onDrag={onDrag} onStop={dragStop}>
+      <div
+        className={styles.tile}
+        ref={containerRef}
+        style={{ transform: `translate(${position[0]}px, ${position[1]}px)` }}
+      >
+        {course.name}
+      </div>
+    </DraggableCore>
+  );
 };
