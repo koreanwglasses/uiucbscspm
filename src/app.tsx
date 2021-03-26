115856c364b354492032e9ec26482f23dda6d2fc
koreanwglasses <koreanwglasses@gmail.com>
2021-03-26 14:18:56 -0400

    read course data

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import styles from "./app.module.css";
+import { Course } from "./model/course";
 
 export function App() {
-  return <div className={styles.container}>Hello World</div>;
+  const coursesRef = React.useRef<Course[]>();
+  React.useEffect(() => {
+    (async () => {
+      const response = await fetch("/static/courses.json");
+      coursesRef.current = await response.json();
+    })();
+  }, []);
+
+  return <div className={styles.container}>Hello World!</div>;
 }
