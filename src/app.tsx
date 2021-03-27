04f7a3003bb47075c13cc65e2175856db6d20d15
koreanwglasses <koreanwglasses@gmail.com>
2021-03-26 20:18:09 -0400

    tiles are draggable into the schedule

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,38 @@
 import * as React from "react";
 import "./layout.css";
 import styles from "./app.module.css";
-import { TileEventHandler } from "./components/course-tile";
+import { DropEvent, TileEventHandler } from "./components/course-tile";
 import { Palette } from "./components/palette";
-import { Schedule } from "./components/schedule";
+import { getCellAt, Schedule } from "./components/schedule";
 import { useCourses } from "./hooks/use-courses";
+import { Course, CourseSelection } from "./model/course";
 
 export const App: React.FC = () => {
   const courses = useCourses();
+  const [selectedCourses, setSelectedCourses] = React.useState<
+    CourseSelection[]
+  >([]);
+
+  const handleDropOnSchedule = (event: DropEvent) => {
+    const newCourseSelection: CourseSelection = {
+      ...getCellAt(event.parentEvent.clientX, event.parentEvent.clientY),
+      course: event.course,
+    };
+
+    setSelectedCourses([
+      ...selectedCourses.filter(
+        (selectedCourse) => selectedCourse.course.id !== event.course.id
+      ),
+      newCourseSelection,
+    ]);
+  };
 
   const handleTileEvent: TileEventHandler = (event) => {
     if (event.type === "drop") {
       console.log(`Dropped ${event.course.id} onto ${event.target}`);
+      if (event.target === "schedule") {
+        handleDropOnSchedule(event);
+      }
     }
     return false;
   };
@@ -20,12 +41,15 @@ export const App: React.FC = () => {
     <div className={styles.container}>
       <div className={styles.panelsContainer}>
         <div className={styles.scheduleContainer}>
-          <Schedule selectedCourses={[]} onTileEvent={handleTileEvent} />
+          <Schedule
+            selectedCourses={selectedCourses}
+            onTileEvent={handleTileEvent}
+          />
         </div>
         <div className={styles.paletteContainer}>
           <Palette
             courses={courses}
-            selectedCourses={[]}
+            selectedCourses={selectedCourses}
             onTileEvent={handleTileEvent}
           />
         </div>
--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -4,6 +4,59 @@ import { range } from "../utils";
 import { CourseTile, TileEventHandler } from "./course-tile";
 import styles from "./schedule.module.css";
 
+const semesters: { semester: "fall" | "spring"; year: number }[] = [
+  {
+    semester: "fall",
+    year: 2021,
+  },
+  {
+    semester: "spring",
+    year: 2022,
+  },
+  {
+    semester: "fall",
+    year: 2022,
+  },
+  {
+    semester: "spring",
+    year: 2023,
+  },
+  {
+    semester: "fall",
+    year: 2023,
+  },
+  {
+    semester: "spring",
+    year: 2024,
+  },
+  {
+    semester: "fall",
+    year: 2024,
+  },
+  {
+    semester: "spring",
+    year: 2025,
+  },
+];
+
+export const getCellAt = (clientX: number, clientY: number) => {
+  const elements = document.elementsFromPoint(clientX, clientY);
+
+  const cell = elements.find((elem) => elem.classList.contains(styles.cell));
+
+  if (!cell) return;
+
+  const row = cell.parentElement;
+  const position = [...row.children].indexOf(cell) - 1;
+  const rowIndex = [...row.parentElement.children].indexOf(row);
+  const { semester, year } = semesters[rowIndex];
+
+  return { semester, year, position };
+};
+
+export const isSchedule = (element: HTMLElement) =>
+  element.classList.contains(styles.schedule);
+
 export const Schedule: React.FC<{
   selectedCourses: CourseSelection[];
   onTileEvent: TileEventHandler;
@@ -14,48 +67,7 @@ export const Schedule: React.FC<{
     selectedCourses: CourseSelection[];
   };
 
-  const rows: Row[] = [
-    {
-      semester: "fall",
-      year: 2021,
-      selectedCourses: [],
-    },
-    {
-      semester: "spring",
-      year: 2022,
-      selectedCourses: [],
-    },
-    {
-      semester: "fall",
-      year: 2022,
-      selectedCourses: [],
-    },
-    {
-      semester: "spring",
-      year: 2023,
-      selectedCourses: [],
-    },
-    {
-      semester: "fall",
-      year: 2023,
-      selectedCourses: [],
-    },
-    {
-      semester: "spring",
-      year: 2024,
-      selectedCourses: [],
-    },
-    {
-      semester: "fall",
-      year: 2024,
-      selectedCourses: [],
-    },
-    {
-      semester: "spring",
-      year: 2025,
-      selectedCourses: [],
-    },
-  ];
+  const rows: Row[] = semesters.map((sem) => ({ ...sem, selectedCourses: [] }));
 
   selectedCourses.forEach((selectedCourse) =>
     rows
@@ -70,31 +82,33 @@ export const Schedule: React.FC<{
   return (
     <div className={styles.schedule}>
       <h1>Schedule</h1>
-      {rows.map(({ semester, year, selectedCourses }) => (
-        <div className={styles.row} key={`${year}-${semester}`}>
-          <p>
-            {semester} {year}
-          </p>
-          {range(
-            Math.max(5, ...selectedCourses.map(({ position }) => position))
-          ).map((i) => {
-            const selectedCourse = selectedCourses.find(
-              ({ position }) => position === i
-            );
-            return (
-              <div className={styles.cell} key={i}>
-                {selectedCourse && (
-                  <CourseTile
-                    course={selectedCourse.course}
-                    onTileEvent={onTileEvent}
-                    key={selectedCourse.course.id}
-                  />
-                )}
-              </div>
-            );
-          })}
-        </div>
-      ))}
+      <div>
+        {rows.map(({ semester, year, selectedCourses }) => (
+          <div className={styles.row} key={`${year}-${semester}`}>
+            <p>
+              {semester} {year}
+            </p>
+            {range(
+              Math.max(5, ...selectedCourses.map(({ position }) => position))
+            ).map((i) => {
+              const selectedCourse = selectedCourses.find(
+                ({ position }) => position === i
+              );
+              return (
+                <div className={styles.cell} key={i}>
+                  {selectedCourse && (
+                    <CourseTile
+                      course={selectedCourse.course}
+                      onTileEvent={onTileEvent}
+                      key={selectedCourse.course.id}
+                    />
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
