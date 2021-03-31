639bafd940e84efbaa95edd20548d20cda31a80d
koreanwglasses <koreanwglasses@gmail.com>
2021-03-31 00:29:57 -0400

    draw connectors between prerequisities

---
+++ src/components/connector.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+
+export const Connector: React.FC<{
+  start: HTMLElement;
+  end: HTMLElement;
+}> = ({ start, end }) => {
+  const startRect = start.getBoundingClientRect();
+  const startPosition = {
+    x: scrollX + startRect.left + startRect.width / 2,
+    y: scrollY + startRect.bottom - 12,
+  };
+
+  const endRect = end.getBoundingClientRect();
+  const endPosition = {
+    x: scrollX + endRect.left + endRect.width / 2,
+    y: scrollY + endRect.top + 7,
+  };
+
+  const bounds = {
+    left: Math.min(startPosition.x, endPosition.x),
+    top: Math.min(startPosition.y, endPosition.y),
+    width: Math.abs(endPosition.x - startPosition.x),
+    height: Math.abs(endPosition.y - startPosition.y),
+  };
+
+  const padding = 10;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: "absolute",
+        left: bounds.left - padding,
+        top: bounds.top - padding,
+      }}
+      width={bounds.width + 2 * padding}
+      height={bounds.height + 2 * padding}
+    >
+      <defs>
+        <marker
+          id="arrowhead"
+          markerWidth="10"
+          markerHeight="7"
+          refX="0"
+          refY="3.5"
+          orient="auto"
+        >
+          <polygon points="0 0, 10 3.5, 0 7" />
+        </marker>
+      </defs>
+      <line
+        x1={padding + (startPosition.x < endPosition.x ? 0 : bounds.width)}
+        y1={padding + (startPosition.y < endPosition.y ? 0 : bounds.height)}
+        x2={padding + (startPosition.x < endPosition.x ? bounds.width : 0)}
+        y2={padding + (startPosition.y < endPosition.y ? bounds.height : 0)}
+        stroke="#000"
+        strokeWidth="1"
+        markerEnd="url(#arrowhead)"
+      />
+    </svg>
+  );
+};
--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { CourseSelection } from "../model/course";
-import { range } from "../utils";
+import { JSONMap, range } from "../utils";
+import { Connector } from "./connector";
 import { CourseTile, TileEventHandler } from "./course-tile";
 import styles from "./schedule.module.css";
 
@@ -39,7 +40,9 @@ const semesters: { semester: "fall" | "spring"; year: number }[] = [
   },
 ];
 
-export const getCellAt = (clientX: number, clientY: number) => {
+export type CellData = { semester: string; year: number; position: number };
+
+export const getCellAt = (clientX: number, clientY: number): CellData => {
   const elements = document.elementsFromPoint(clientX, clientY);
 
   const cell = elements.find((elem) => elem.classList.contains(styles.cell));
@@ -79,6 +82,8 @@ export const Schedule: React.FC<{
       .selectedCourses.push(selectedCourse)
   );
 
+  const cellRefs = React.useRef(new JSONMap<CellData, HTMLDivElement>());
+
   return (
     <div className={styles.schedule}>
       <h2>Schedule</h2>
@@ -90,9 +95,33 @@ export const Schedule: React.FC<{
             semester={semester}
             selectedCourses={selectedCourses}
             onTileEvent={onTileEvent}
+            onCellRef={(cell, ref) => cellRefs.current.set(cell, ref)}
           />
         ))}
       </div>
+      {selectedCourses.map((selectedCourse, i) => {
+        const prequisiteSelections = selectedCourses.filter(
+          ({ course }) =>
+            selectedCourse.course.prereqs.includes(course.id.slice(0, 5)) // TODO: make a more robust prereq searcher
+        );
+
+        return prequisiteSelections.map((prequisiteSelection, j) => {
+          const start = cellRefs.current.get({
+            semester: prequisiteSelection.semester,
+            year: prequisiteSelection.year,
+            position: prequisiteSelection.position,
+          });
+          const end = cellRefs.current.get({
+            semester: selectedCourse.semester,
+            year: selectedCourse.year,
+            position: selectedCourse.position,
+          });
+          return (
+            start &&
+            end && <Connector start={start} end={end} key={`${i},${j}`} />
+          );
+        });
+      })}
     </div>
   );
 };
@@ -102,44 +131,52 @@ const Row = ({
   semester,
   selectedCourses,
   onTileEvent,
+  onCellRef,
 }: {
   year: number;
   semester: string;
   selectedCourses: CourseSelection[];
   onTileEvent: TileEventHandler;
+  onCellRef: (cell: CellData, ref: HTMLDivElement) => void;
 }) => (
   <div className={styles.row} key={`${year}-${semester}`}>
     <p>
       {semester} {year}
     </p>
     {range(Math.max(5, ...selectedCourses.map(({ position }) => position))).map(
-      (i) => (
+      (position) => (
         <Cell
-          key={i}
+          key={position}
           selectedCourse={selectedCourses.find(
-            ({ position }) => position === i
+            (selectedCourse) => selectedCourse.position === position
           )}
           onTileEvent={onTileEvent}
+          ref={(ref) => onCellRef({ semester, year, position }, ref)}
         />
       )
     )}
   </div>
 );
 
-const Cell = ({
-  selectedCourse,
-  onTileEvent,
-}: {
-  selectedCourse: CourseSelection;
-  onTileEvent: TileEventHandler;
-}) => (
-  <div className={styles.cell}>
-    {selectedCourse && (
-      <CourseTile
-        course={selectedCourse.course}
-        onTileEvent={onTileEvent}
-        key={selectedCourse.course.id}
-      />
-    )}
-  </div>
+const Cell = React.forwardRef(
+  (
+    {
+      selectedCourse,
+      onTileEvent,
+    }: {
+      selectedCourse: CourseSelection;
+      onTileEvent: TileEventHandler;
+    },
+    ref: React.ForwardedRef<HTMLDivElement>
+  ) => (
+    <div className={styles.cell} ref={ref}>
+      {selectedCourse && (
+        <CourseTile
+          course={selectedCourse.course}
+          onTileEvent={onTileEvent}
+          key={selectedCourse.course.id}
+        />
+      )}
+    </div>
+  )
 );
